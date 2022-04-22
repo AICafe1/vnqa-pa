c8c88c921ebcefe87d01c9cbb0f12ef872747447
Nguyen Dac Doan <nguyendacdoan@gmail.com>
2022-04-22 15:14:35 +0700

    PA

---
+++ docs/precache-manifest.8c70928f3b8f58d2f34c95df98ffcc2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7167d2782116d5f7d956a62aaf494b99",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7e03a9e619ae8e341538",
+    "url": "./static/css/2.5ee7db17.chunk.css"
+  },
+  {
+    "revision": "7e03a9e619ae8e341538",
+    "url": "./static/js/2.3a6448a9.chunk.js"
+  },
+  {
+    "revision": "affaead7a68134b541afe1da4cf55fab",
+    "url": "./static/js/2.3a6448a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d7269ce95b17a8fdce9",
+    "url": "./static/js/main.ed197680.chunk.js"
+  },
+  {
+    "revision": "ab40382ca97013f04a71",
+    "url": "./static/js/runtime-main.6932692b.js"
+  }
+]);
